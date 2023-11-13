50b507d88f68ff85948fba1781679559cab89779
Riccardo Vukovic <s320029@studenti.polito.it>
2023-11-13 15:53:19 +0100

    adjustments

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function MyNavbar() {
                         {store.user.type === 'student'? <Nav.Link className='text-white' href="/">Thesis</Nav.Link> : <Nav.Link  className='text-white' href="/">Applications</Nav.Link>}
                         {store.user.type === 'student'? <Nav.Link className='text-white' href="/">My Applications</Nav.Link> : <Nav.Link style={{marginLeft:'30px'}} className='text-white' href="/insertProposal">My Proposals</Nav.Link>}
                     </Nav>
-                    <NavDropdown style={{marginLeft:'35%'}} title={
+                    <NavDropdown className='dropdown-toggle-myicon' style={{marginLeft:'35%'}} title={
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-person-circle" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                             <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
--- client/src/components/ThesisList.jsx
+++ client/src/components/ThesisList.jsx
@@ -4,30 +4,49 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Stack from 'react-bootstrap/Stack'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function ThesisList(props) {
 
     return (
         <>
         <MyNavbar></MyNavbar>
-      
-            <Container fluid  className="border-thesis-div" style={{paddingTop:'10%'}}> 
-                <Row>
-                    <Col className="border-thesis-div" md={{span:9, offset:3}}>
-                        <Col lg={3}>
-                        <h1 className="border-thesis-row">type + search</h1>
-                        </Col>
-                       
+            <Container style={{paddingTop:'10%'}}>  
+                <Row className="justify-content-between thesis-form-section">
+                    <Col className="d-flex justify-content-around" lg={{span:10, offset:2}}>
+                        <DropdownButton variant="light" id="dropdown-item-button" title="Grade Type">
+                            <Dropdown.Header>Grade</Dropdown.Header>
+                            <Dropdown.Item as="button">Bachelor</Dropdown.Item>
+                            <Dropdown.Item as="button">Master</Dropdown.Item>
+                            <Dropdown.Item as="button">PHD</Dropdown.Item>
+                        </DropdownButton>
+                        <Form inline='true'>
+                            <Row>
+                                <Col xs="auto">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Search"
+                                        className=" mr-sm-2"
+                                    />
+                                </Col>
+                                <Col xs="auto">
+                                    <Button variant='light' type="submit">Submit</Button>
+                                </Col>
+                            </Row>
+                        </Form>
                     </Col>
                 </Row>
-                <Row>
-                    <Col>
+                <Row className="justify-content-start border-thesis-div">
+                    <Col className="d-flex justify-content-center border-thesis-filter">
                         <h2>order by</h2>
                     </Col>
-                    <Col lg={9}>
-                        <div className="my-4 border-thesis-row">
-                            <header >
-                                <h2>Schools, Departments &amp; the College</h2>
+                    <Col lg={10}>
+                        <div className="thesis-section">
+                            <header>
+                                <h2 className="border-thesis-title">Schools, Departments &amp; the College</h2>
                             </header>
                             <div >
                                 <div >
@@ -37,13 +56,13 @@ function ThesisList(props) {
                                 </div>
                             </div>
                         </div>
-                        <div className="my-4 border-thesis-row">
-                            <header >
-                                <h2>Schools, Departments &amp; the College</h2>
+                        <div className="thesis-section">
+                            <header>
+                                <h2 className="border-thesis-title">Schools, Departments &amp; the College</h2>
                             </header>
                             <div >
                                 <div >
-                                    <p>AAAAAAAAAAAAAAAAAAAAAThey are pioneering scholars who love to teach. Deeply engaged in practice, they&nbsp;topple conventional walls between fields in the push for deeper understanding and fresh ideas. In fact, many&nbsp;faculty&nbsp;actively work in at least one of MIT’s&nbsp;interdisciplinary labs, centers, initiatives, and institutes that target crucial challenges, from <a href="http://energy.mit.edu/" target="_blank">clean energy</a> to <a href="https://ki.mit.edu/" target="_blank">cancer</a>.</p>
+                                    <p>Across MIT, faculty help set the global standard of excellence in their disciplines: They are pioneering scholars who love to teach. Deeply engaged in practice, they&nbsp;topple conventional walls between fields in the push for deeper understanding and fresh ideas. In fact, many&nbsp;faculty&nbsp;actively work in at least one of MIT’s&nbsp;interdisciplinary labs, centers, initiatives, and institutes that target crucial challenges, from <a href="http://energy.mit.edu/" target="_blank">clean energy</a> to <a href="https://ki.mit.edu/" target="_blank">cancer</a>.</p>
                                     <p>The MIT Schwarzman College of Computing, opened in fall 2019, is a cross-cutting entity with education and research links across all five schools.</p>
                                     <p><a  href="/education/schools-and-departments/">Explore Departments</a></p>
                                 </div>
