711847b6a6af16dba48467f552dab0963c3b4d29
Rick <riccardo.vukovic@gmail.com>
2023-11-14 11:00:39 +0100

    dynamic rendering of the thesis list

--- client/src/components/ThesisList.jsx
+++ client/src/components/ThesisList.jsx
@@ -11,12 +11,24 @@ import Nav from 'react-bootstrap/Nav'
 
 function ThesisList(props) {
 
+    const thesisList = [
+        {
+            id: 1,
+            title: 'THESIS 1',
+            description: `Across MIT, faculty help set the global standard of excellence in their disciplines: They are pioneering scholars who love to teach. Deeply engaged in practice, they&nbsp;topple conventional walls between fields in the push for deeper understanding and fresh ideas. In fact, many&nbsp;faculty&nbsp;actively work in at least one of MIT’s&nbsp;interdisciplinary labs, centers, initiatives, and institutes that target crucial challenges, from <a href="http://energy.mit.edu/" target="_blank">clean energy</a> to <a href="https://ki.mit.edu/" target="_blank">cancer</a>.`
+        },
+        {
+            id: 2,
+            title: 'THESIS 232323',
+            description: 'The MIT Schwarzman College of Computing, opened in fall 2019, is a cross-cutting entity with education and research links across all five schools.'
+        }
+    ]
     return (
         <>
-        <MyNavbar></MyNavbar>
-            <Container fluid className="under-nav">  
+            <MyNavbar></MyNavbar>
+            <Container fluid className="under-nav">
                 <Row className="justify-content-between thesis-form-section">
-                    <Col className="d-flex justify-content-around" lg={{span:10, offset:2}}>
+                    <Col className="d-flex justify-content-around" lg={{ span: 8, offset: 2 }}>
                         <DropdownButton variant="light" id="dropdown-item-button" title="Grade Type">
                             <Dropdown.Header>Grade</Dropdown.Header>
                             <Dropdown.Item as="button">Bachelor</Dropdown.Item>
@@ -40,8 +52,8 @@ function ThesisList(props) {
                     </Col>
                 </Row>
                 <Row className="border-thesis-div">
-                    <Col className="d-flex justify-content-center border-thesis-filter">
-                          <Nav variant="underline" className="justify-content-center flex-column">
+                    <Col lg={2} className="d-flex justify-content-center border-thesis-filter">
+                        <Nav variant="underline" className="justify-content-center flex-column">
                             <Nav.Item className="d-inline-flex">
                                 <Nav.Link className="filter-decoration" eventKey='research'>By Research Group</Nav.Link>
                             </Nav.Item>
@@ -54,36 +66,27 @@ function ThesisList(props) {
                             <Nav.Item className="d-inline-flex">
                                 <Nav.Link className="filter-decoration" eventKey="sub">By Subject</Nav.Link>
                             </Nav.Item>
-                          </Nav>
+                        </Nav>
                     </Col>
-                    <Col lg={10}>
-                        <div className="thesis-section">
-                            <header>
-                                <h2 className="border-thesis-title"><Nav.Link href="/thesis">THESIS 1</Nav.Link></h2>
-                            </header>
-                            <div >
-                                <div >
-                                    <p>Across MIT, faculty help set the global standard of excellence in their disciplines: They are pioneering scholars who love to teach. Deeply engaged in practice, they&nbsp;topple conventional walls between fields in the push for deeper understanding and fresh ideas. In fact, many&nbsp;faculty&nbsp;actively work in at least one of MIT’s&nbsp;interdisciplinary labs, centers, initiatives, and institutes that target crucial challenges, from <a href="http://energy.mit.edu/" target="_blank">clean energy</a> to <a href="https://ki.mit.edu/" target="_blank">cancer</a>.</p>
-                                    <p>The MIT Schwarzman College of Computing, opened in fall 2019, is a cross-cutting entity with education and research links across all five schools.</p>
-                                    <p><a className="border-thesis-view">VIEW</a></p>
+                    <Col lg={8}>
+                        {
+                            thesisList.map((e) =>
+                                <div key={e.id} className="thesis-section">
+                                    <header>
+                                        <h2 className="border-thesis-title"><Nav.Link href="/thesis">{e.title}</Nav.Link></h2>
+                                    </header>
+                                    <div >
+                                        <div >
+                                            <p>{e.description}</p>
+                                            <p><a className="border-thesis-view">VIEW</a></p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="thesis-section">
-                            <header>
-                                <h2 className="border-thesis-title">Schools, Departments &amp; the College</h2>
-                            </header>
-                            <div >
-                                <div >
-                                    <p>Across MIT, faculty help set the global standard of excellence in their disciplines: They are pioneering scholars who love to teach. Deeply engaged in practice, they&nbsp;topple conventional walls between fields in the push for deeper understanding and fresh ideas. In fact, many&nbsp;faculty&nbsp;actively work in at least one of MIT’s&nbsp;interdisciplinary labs, centers, initiatives, and institutes that target crucial challenges, from <a href="http://energy.mit.edu/" target="_blank">clean energy</a> to <a href="https://ki.mit.edu/" target="_blank">cancer</a>.</p>
-                                    <p>The MIT Schwarzman College of Computing, opened in fall 2019, is a cross-cutting entity with education and research links across all five schools.</p>
-                                    <p><a  href="/education/schools-and-departments/">Explore Departments</a></p>
-                                </div>
-                            </div>
-                        </div>
+                            )
+                        }
                     </Col>
                 </Row>
-            </Container>    
+            </Container>
         </>
     )
 }
