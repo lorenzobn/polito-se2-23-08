9839961ead6b65e812880a3a37972a9cba68e915
Dani <daniad7@outlook.com>
2023-11-20 16:48:48 +0100

    minor ui improvements

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
-
+import politoImg from "../../images/polito.jpeg";
 function MyNavbar() {
   const store = useContext(StoreContext);
   const navigate = useNavigate();
