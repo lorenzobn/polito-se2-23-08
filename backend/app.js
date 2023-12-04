2fc52e9fcd23993cd38d40bd646ed662cd1a717a
Rick <riccardo.vukovic@gmail.com>
2023-12-04 12:53:07 +0100

    port adjustment

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ const session = require("express-session");
 // CORS options to access APIs
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   credentials: true,
 };
 
