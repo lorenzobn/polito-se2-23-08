cc92a1f55ab4d053fd80eb23b4b326fe274d5856
Dani <daniad7@outlook.com>
2024-01-16 07:19:51 +0100

    fixed the problem of showing download cv button for the applications without cv

--- client/src/components/DecideApplication.jsx
+++ client/src/components/DecideApplication.jsx
@@ -165,13 +165,15 @@ export default function AcceptApplications() {
               <li>
                 <strong>Student ID:</strong> {student.student_id},{" "}
                 <strong>Application Status:</strong> {student.applicationstatus}
-                <a
-                  className="mt-2 d-block "
-                  style={{ cursor: "pointer", color: "rgb(0, 126, 168)" }}
-                  href={`http://localhost:3000/api/v1.0//received-applications/${student.applicationid}/cv`}
-                >
-                  Download CV
-                </a>
+                {student.cv_uri && (
+                  <a
+                    className="mt-2 d-block "
+                    style={{ cursor: "pointer", color: "rgb(0, 126, 168)" }}
+                    href={`http://localhost:3000/api/v1.0//received-applications/${student.applicationid}/cv`}
+                  >
+                    Download CV
+                  </a>
+                )}
                 <div className="row">
                   <div className="col text-start mt-4">
                     {student.applicationstatus === "idle" ? (
